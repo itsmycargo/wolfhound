4ebb12938f96c14b0d3dcb208de8dd8aeae01309
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2020-11-24 15:05:26 +0100

    fix: reek configuration

--- lib/linter.sh
+++ lib/linter.sh
@@ -3,17 +3,16 @@
 source /action/wolf.sh
 source /action/detect.sh
 
+INTERACTIVE=${INTERACTIVE:="false"}
 VALIDATE_JS="${VALIDATE_JS:=${DETECT_JS}}"
 VALIDATE_PYTHON="${VALIDATE_PYTHON:=${DETECT_PYTHON}}"
 VALIDATE_RUBY="${VALIDATE_RUBY:=${DETECT_RUBY}}"
 VALIDATE_SHELL="${VALIDATE_SHELL:=${DETECT_SHELL}}"
 
-summary() {
-  printf "%25s: (D:%5s R:%5s)\n" "Javascript/Typescript" "${DETECT_JS}" "${VALIDATE_JS}"
-  printf "%25s: (D:%5s R:%5s)\n" "Python" "${DETECT_PYTHON}" "${VALIDATE_PYTHON}"
-  printf "%25s: (D:%5s R:%5s)\n" "Ruby" "${DETECT_RUBY}" "${VALIDATE_RUBY}"
-  printf "%25s: (D:%5s R:%5s)\n" "Shell" "${DETECT_SHELL}" "${VALIDATE_SHELL}"
-}
+if [ -z "${CONTINUOUS_INTEGRATION}" ];
+then
+  INTERACTIVE="true"
+fi
 
 runner() {
   CMD=$1 ; shift
@@ -24,7 +23,7 @@ runner() {
 
   echo "**** Running $CMD"
 
-  if [ -z "$OUTPUT" ];
+  if [ -z "${OUTPUT}"];
   then
     eval "$CMD" "$ARGS"
   else
@@ -34,19 +33,30 @@ runner() {
   echo "Elapsed: $(($SECONDS / 60))min $(($SECONDS % 60))sec"
 }
 
-summary
-
 if [ "${VALIDATE_JS}" = "true" ];
 then
-  runner eslint "--format checkstyle --output-file eslint-result.xml ."
+  if [ "${INTERACTIVE}" = "true" ];
+  then
+    runner eslint "."
+  else
+    runner eslint "--format checkstyle --output-file eslint-result.xml ."
+  fi
 fi
 
 if [ "${VALIDATE_RUBY}" = "true" ];
 then
-  runner /action/fasterer-checkstyle ". fasterer-result.xml"
-  runner /action/flay-checkstyle "flay-result.xml"
-  runner /action/reek-checkstyle ". reek-result.xml"
-  runner rubocop "--require rubocop/formatter/checkstyle_formatter --format RuboCop::Formatter::CheckstyleFormatter --out rubocop-result.xml"
+  if [ "${INTERACTIVE}" = "true" ];
+  then
+    runner fasterer "."
+    runner flay
+    runner reek "."
+    runner rubocop
+  else
+    runner /action/fasterer-checkstyle ". fasterer-result.xml"
+    runner /action/flay-checkstyle "flay-result.xml"
+    runner /action/reek-checkstyle ". reek-result.xml"
+    runner rubocop "--require rubocop/formatter/checkstyle_formatter --format RuboCop::Formatter::CheckstyleFormatter --out rubocop-result.xml"
+  fi
 fi
 
 if [ "${VALIDATE_PYTHON}" = "true" ];
